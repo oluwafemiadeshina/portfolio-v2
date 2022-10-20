ac91f96d3efded8356109050c446e6b71f3ee99b
James <adeshina413@gmail.com>
2022-10-20 16:58:34 +0100

    init

--- js/app.js
+++ js/app.js
@@ -25,3 +25,26 @@ function openNav() {
           $(this).text(i+1).prepend("0");
       });
   });
+  //scrolling feature
+
+  let mybutton = document.getElementById("Backtotop");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+      
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop >= 21) {
+      mybutton.style.display = "block";
+
+    } else {
+      // console.log(document.documentElement.scrollTop);
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
